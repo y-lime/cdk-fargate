8ff1c0ed6261ee943a0a3e2bb0b6869dcf147f4f
y-lime <y.murai.tech@gmail.com>
2023-02-18 14:15:34 +0900

    add vpc

---
+++ lib/fargate-stack-vpc.ts
@@ -0,0 +1,41 @@
+import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import { Construct } from 'constructs';
+
+export class FargateStackVpc extends cdk.Stack {
+    public readonly fargateVpc: ec2.Vpc;
+
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const fargateVpc = new ec2.Vpc(this, 'vpc', {
+            cidr: '20.100.0.0/16',
+            maxAzs: 2,
+            subnetConfiguration: [
+                {
+                    cidrMask: 24,
+                    name: "subnet-private-elb-",
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+                {
+                    cidrMask: 24,
+                    name: "subnet-private-service-",
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+                {
+                    cidrMask: 24,
+                    name: "subnet-private-management-",
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+                {
+                    cidrMask: 24,
+                    name: "subnet-private-db-",
+                    subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+                },
+            ]
+        });
+
+        this.fargateVpc = fargateVpc;
+    }
+
+}
--- lib/fargate-stack.ts
+++ lib/fargate-stack.ts
@@ -1,10 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { FargateStackVpc } from './fargate-stack-vpc';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class FargateStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    // ネットワーク基盤スタック
+    const fargatestackvpc = new FargateStackVpc(this, 'VpcStack');
 
     // The code that defines your stack goes here
 
